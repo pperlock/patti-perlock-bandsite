aa298813d309b319badffd663e41a889b7cbf722
Patti Perlock <pattiperlock@gmail.com>
2020-11-25 17:32:22 -0500

    updated shows page button format and added links to social icons

--- sprint-2/scripts/main.js
+++ sprint-2/scripts/main.js
@@ -1,23 +1,22 @@
 
-// initalize default values for comments array
+// initalize default values for comments array - array is set up in chronological order to allow for .push use
 let comments = [
     {
-        name:"Michael Lyons",
-        timeStamp:"12/18/2018",
-        message:"They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed"
+        name:"Theodore Duncan",
+        timeStamp:"11/15/2018",
+        message: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. everytime I see him I feel instantly happy! He’s definitely my favorite ever!"
     },
-
     {
         name:"Gary Wong",
         timeStamp:"12/12/2018",
         message: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
     },
-
     {
-        name:"Theodore Duncan",
-        timeStamp:"11/15/2018",
-        message: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. everytime I see him I feel instantly happy! He’s definitely my favorite ever!"
+        name:"Michael Lyons",
+        timeStamp:"12/18/2018",
+        message:"They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed"
     }
+
 ]
 
 // This function builds and displays a comment based on the object passed to it
@@ -65,7 +64,10 @@ const displayComment = (newComment) =>{
     commentsList.appendChild(comment);
 }
 
-// loop through each object in the comments array and build and display it
-comments.forEach(comment =>{
-displayComment(comment);
+/* loop through each object in the comments array in reverse chronological order 
+    - build the html element and display it
+    - .slice used to create "copy" of the array without altering original array order*/
+comments.slice().reverse().forEach(comment =>{
+    displayComment(comment);
 });
+
